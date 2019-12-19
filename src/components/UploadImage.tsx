a9d8901dae6c910fc4bb2b70b4bfcaedb7647814
Eddie <eddietseng@outlook.com>
2019-12-19 05:24:31 -0500

    Correctly receives response after making axios request

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react'
 import '../styles/_uploadimage.scss'
+import { createWriteStream } from 'fs'
 const axios = require('axios');
 
 interface MyComponentProps {
@@ -25,13 +26,17 @@ class UploadImage extends Component<MyComponentProps, MyComponentState>{
 
   }
 
-  fileSelectedHandler = (event: any) => {
-    
+  fileSelectedHandler =  (event: any) => {
 
+    //this.setState({... this.state, oldImage: files});
     this.setState({... this.state, oldImage: URL.createObjectURL(event.target.files[0])});
 
     console.log(event.target.files[0])
 
+    let data = new FormData();
+
+    data.append("image", event.target.files[0])
+    
     if (this.props.mode === "train") {
       console.log("Training!")
       axios.get('http://localhost:5000/api/train')
@@ -46,17 +51,27 @@ class UploadImage extends Component<MyComponentProps, MyComponentState>{
     }
 
     if (this.props.mode === "identify") {
-      axios({
+      var self = this;
+       axios({
         method: 'post',
         url:'http://localhost:5000/api/recognize',
-        data: {
-          image: event.target.files[0]
+        data: data,
+        responseType: 'arraybuffer',
+        responseEncoding: 'binary',
+        headers:{
+          'Content-Type': 'multipart/form-data',
+          encoding: null
         }
       })
 
         .then(function (response:any) {
-          // handle success
+          console.log(response.data)
           console.log(response);
+          
+          let blob = new Blob( [ response.data ], { type: "image/jpeg" } );
+
+          self.setState({... self.state, newImage: URL.createObjectURL(blob)});
+          
         })
         .catch(function (error:any) {
           // handle error
@@ -78,6 +93,7 @@ class UploadImage extends Component<MyComponentProps, MyComponentState>{
    return(
      <Fragment>
        <input
+         id='image'
          type="file"
          onChange={this.fileSelectedHandler}
          name="image"
@@ -85,6 +101,7 @@ class UploadImage extends Component<MyComponentProps, MyComponentState>{
        {/* <h2>Selected File: {this.state.selectedFile}</h2> */}
        <div className="images">
         <img src={this.state.oldImage}></img>
+
         <img src={this.state.newImage}></img>
        </div>
      </Fragment>
